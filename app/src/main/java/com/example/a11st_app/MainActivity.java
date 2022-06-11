cab27d099e0b00aa6066a2047d83dce2665ebb2f
eomsueun <ese7142@naver.com>
2022-06-12 01:52:04 +0900

    영화정보의수 추가_기말고사

--- app/src/main/java/com/example/a11st_app/MainActivity.java
+++ app/src/main/java/com/example/a11st_app/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.a11st_app;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.android.volley.AuthFailureError;
@@ -15,6 +19,7 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.gson.Gson;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -25,24 +30,36 @@ public class MainActivity extends AppCompatActivity {
 
     static RequestQueue requestQueue;
 
+    RecyclerView recyclerView;
+    MovieAdapter adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         requestText = findViewById(R.id.requestText);
-        responseText = findViewById(R.id.responseText);
+        recyclerView = findViewById(R.id.recyclerView);
+
 
         Button requestBtn = findViewById(R.id.requestBtn);
-        requestBtn.setOnClickListener((new View.OnClickListener() {
+        requestBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 makeRequest();
-            }
-        }));
 
+            }
+        });
         if (requestQueue == null){
             requestQueue = Volley.newRequestQueue(getApplicationContext());
         }
+        recyclerView = findViewById(R.id.recyclerView);
+        LinearLayoutManager layoutManager = new LinearLayoutManager
+                (this,LinearLayoutManager.VERTICAL, false);
+        recyclerView.setLayoutManager(layoutManager);
+
+        adapter = new MovieAdapter();
+        recyclerView.setAdapter(adapter);
     }
 
     public void makeRequest(){
@@ -60,13 +77,13 @@ public class MainActivity extends AppCompatActivity {
                 new Response.ErrorListener() {
                     @Override
                     public void onErrorResponse(VolleyError error) {
-                        println("에러 -> " + error.getMessage());
+                        println("에러 - >" + error.getMessage());
                     }
                 }
         ){
             @Override
             protected Map<String, String> getParams() throws AuthFailureError{
-                Map<String,String> params = new HashMap<String,String>();
+                Map<String,String> params = new HashMap<String, String>();
                 return params;
             }
         };
@@ -74,12 +91,21 @@ public class MainActivity extends AppCompatActivity {
         requestQueue.add(request);
         println("요청보냄");
     }
-    public void println(String data){
-        responseText.append(data + "\n");
+    public void println(String data)  {
+        Log.d("MainActivity",data);
     }
-    public void processResponse(String response){
+       public void processResponse(String response)
+    {
         Gson gson = new Gson();
         MovieList movieList = gson.fromJson(response, MovieList.class);
-        println("영화 정보의 수 : " + movieList.boxOfficeResult.dailyBoxOfficeList.size());
+        println("영화 정보의 수: " +  movieList.boxOfficeResult.dailyBoxOfficeList.size());
+
+        for(int i=0; i< movieList.boxOfficeResult.dailyBoxOfficeList.size();i++) {
+           Movie movie = movieList.boxOfficeResult.dailyBoxOfficeList.get(i);
+           adapter.addItem(movie);
+        }
+        adapter.notifyDataSetChanged();
     }
+
 }
+
